f2c236935e2e9a815e57dd47b0fcf6b97a814c17
Anna Mendrukh <amendrukh@gmail.com>
2023-02-22 15:58:22 +0200

    /hw3.js has been changed.

--- hw3.js
+++ hw3.js
@@ -47,3 +47,81 @@ function giveMeFive(obj) {
 console.log(giveMeFive({Our: "earth", is: "a", beautyful: "world"})); //["earth","world"]
 console.log(giveMeFive({Ihave: "enough", money: "to", buy: "a", car: "model"})); //["Ihave","money","model"]
 console.log(giveMeFive({Pears: "than", apple: "sweet"})); //["Pears","apple","sweet"]
+
+//https://www.codewars.com/kata/understanding-closures-the-basics/train/javascript
+//Это простое упражнение для понимания функции языка javascript, называемой замыканием. Функция buildFun вернет массив
+// функций. Единственным параметром, принимаемым buildFun, является количество элементов N возвращаемого массива.
+// Желаемый результат заключается в том, что при выполнении всех функций в массиве должно быть возвращено число
+// от 0 до N.
+
+function buildFun(n) {
+    let res = []
+
+    for (let i = 0; i < n; i++) {
+        res.push(function () {
+            return i
+        })
+    }
+    return res
+}
+
+console.log(buildFun(10));
+
+//https://www.codewars.com/kata/fun-with-es6-classes-number-2-animals-and-inheritance/train/javascript
+
+class Animal {
+    constructor(name, age, legs, species, status) {
+        this.name = name;
+        this.age = age;
+        this.legs = legs;
+        this.species = species;
+        this.status = status;
+    }
+
+    introduce() {
+        return `Hello, my name is ${this.name} and I am ${this.age} years old.`;
+    }
+}
+
+class Shark extends Animal {
+    constructor(name, age, status) {
+        super(name, age, 0, "shark", status);
+    }
+}
+
+let billy = new Shark("Billy", 3, "Alive and well");
+console.log(billy)
+
+class Cat extends Animal {
+    constructor(name, age, status) {
+        super(name, age, 4, "cat", status)
+    }
+
+    introduce() {
+        return super.introduce() + `  Meow meow!`;
+    }
+}
+
+let cathy = new Cat("Cathy", 7, "Playing with a ball of yarn");
+console.log(cathy);
+console.log(cathy.introduce())
+
+
+class Dog extends Animal {
+    constructor(name, age, status, master) {
+        super(name, age, 4, "dog", status);
+        this.master = master;
+    }
+
+    introduce() {
+        return super.introduce();
+    }
+
+    greetMaster() {
+        return `Hello ${this.master}`
+    }
+}
+
+let doug = new Dog("Doug", 12, "Serving his master", "Eliza");
+console.log(doug)
+console.log(doug.greetMaster())
